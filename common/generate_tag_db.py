07fb9ece75a1f442f78b70b27c0d5b9fba01df2b
Kyoichi Iwasaki <kyiwasak@microsoft.com>
2022-07-08 09:25:17 +0900

    Added main apps

---
+++ common/generate_tag_db.py
@@ -0,0 +1,123 @@
+import os
+import pandas as pd
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+
+class generate_tag_db():
+    '''
+    The class provides functions, where you can search analyzed tags for each image.    
+    '''
+    def __init__(self
+                ,subscription_key
+                ,cv_endpoint
+                ,language
+                ,features
+                ,tag_db_file
+                ,tag_db_columns
+                ,upload_dir):
+        # Azure Subscription
+        self.subscription_key = subscription_key
+        # Azure Computer Vision(CV)
+        ## Endpoint of CV
+        self.cv_endpoint = cv_endpoint
+        ## Set language in CV
+        self.language = language
+        ## Set features to be extracted in CV
+        self.features = features
+        # Set tag db file, which stores tags predicted by Azure CV 
+        self.tag_db_file = tag_db_file
+        # Set columns for tag db file
+        self.tag_db_columns = tag_db_columns
+        # tag db
+        self.df_tag_db = None
+        # Set directory for storing images
+        self.upload_dir = upload_dir
+        # Set how many images are shown in display
+        self.topN = 5
+
+    def read_image(self
+                    ,wkdir : str
+                    ,image_file : str):
+        '''
+        - Input
+            wkdir: path to image
+            image_file: file name of image
+        - Process
+            Open the image file
+        - Output
+            Stream I/O of image file        
+        '''
+        read_image_path = os.path.join(wkdir, image_file)
+        return open(read_image_path, "rb")
+
+    def load_tag_db(self) -> pd.DataFrame():
+        '''
+        - Process
+            Load/Initialize tag db
+        - Output
+            tag db
+        '''
+        if os.path.exists(self.tag_db_file):
+            print('file exists!')
+            df_tag_db = pd.read_pickle(self.tag_db_file)
+        else:
+            df_tag_db = pd.DataFrame([], columns=self.tag_db_columns)
+        return df_tag_db
+
+    def generate_tag_database(self
+                    ,image_file: str) -> pd.DataFrame():
+        '''
+        - Input
+            image_file: file name of image
+        - Process
+            After loading image, consume Azure CV, and parse the results
+        - Output
+            Parsed tag information predicted by Azure CV
+        '''
+        ## Load image
+        image_data = self.read_image(self.upload_dir, image_file)
+
+        ## Analyze image with Computer Vision
+        computervision_client = ComputerVisionClient(self.cv_endpoint
+                                                ,CognitiveServicesCredentials(self.subscription_key))
+        results_local = computervision_client.analyze_image_in_stream(image_data
+                                                                    ,self.features
+                                                                    ,language=self.language)
+
+        ## extract tags
+        list_tag_db = [[image_file, tag.name, tag.confidence] for tag in results_local.tags]
+        return pd.DataFrame(list_tag_db, columns=self.tag_db_columns)
+
+    def analyze_insert_tag(self
+                        ,image_file: str) -> None:
+        '''
+        - Input
+            image_file: file name of image
+        - Process
+            - Load tag db, if exists
+            - Analyze image with Azure CV
+            - Insert the analyzed results into tag db
+        - Output
+            Dataframe with analyzed tags in full
+        '''
+        ## Load tag_db 
+        self.df_tag_db = self.load_tag_db()
+        self.df_tag_db = self.df_tag_db[self.df_tag_db['image_name'] != image_file]
+
+        ## Analyze image with Azure CV
+        df_tag_db_tmp = self.generate_tag_database(image_file)
+
+        ## insert records into tag db
+        if os.path.exists(self.tag_db_file):
+            df_tag_db = pd.read_pickle(self.tag_db_file)
+            df_tag_db = pd.concat([df_tag_db, df_tag_db_tmp], axis=0)
+        else:
+            df_tag_db = df_tag_db_tmp
+        df_tag_db = df_tag_db.reset_index(drop=True)
+        df_tag_db.to_pickle(self.tag_db_file)
+        return df_tag_db
+
+    def extract_records_from_db_with_word(self
+                                        ,word) -> list:
+        self.df_tag_db =self.load_tag_db()
+        return self.df_tag_db[self.df_tag_db['tag'] == word].sort_values(self.tag_db_columns[2], ascending=False)
---
+++ st_app.py
@@ -0,0 +1,92 @@
+import streamlit as st
+import configparser
+from common.generate_tag_db import *
+
+## Setting
+config_ini = configparser.ConfigParser()
+config_ini.read('./common/config.ini', encoding='utf-8')
+
+### Azure Subscription
+subscription_key = config_ini['Azure']['subscription_key']
+
+### Azure Computer Vision
+cv_endpoint = config_ini['Azure']['cv_endpoint']
+language = config_ini['Azure']['language']
+features = ['objects', 'tags']
+
+### tag db, upload directory
+tag_db_file = 'df_tag_db.pkl'
+tag_db_columns = ['image_name', 'tag', 'confidence']
+upload_dir = './upload_images'
+
+## Read class
+gt = generate_tag_db(subscription_key=subscription_key
+                    ,cv_endpoint=cv_endpoint
+                    ,language=language
+                    ,features=features
+                    ,tag_db_file=tag_db_file
+                    ,tag_db_columns=tag_db_columns
+                    ,upload_dir=upload_dir)
+
+with st.sidebar:
+    word = st.text_input('Search words', 'type your words')
+
+    topN = st.number_input(label='Insert a number for topN'
+                            ,min_value=3
+                            ,max_value=20
+                            ,step = 1)
+    search_result_N = st.number_input(label='Input number of search results'
+                            ,min_value=3
+                            ,max_value=10
+                            ,step=1)
+    gt.topN = int(topN)
+
+    uploaded_file = st.file_uploader("Choose a file to store your image", type=['jpg', 'jpeg', 'png'])
+    if uploaded_file is not None:
+        ## File exist?
+        saved_image_files = os.listdir(gt.upload_dir)
+        if uploaded_file.name in saved_image_files:
+            st.warning('File already exists!')
+        else:
+            image_path = os.path.join(gt.upload_dir, uploaded_file.name)
+            ## Save image
+            with open(image_path, 'wb') as f:
+                f.write(uploaded_file.read())
+
+            df_tag_db = gt.analyze_insert_tag(uploaded_file.name)
+            st.info(f'Saved {uploaded_file.name}, and analyzed results are ..')
+            st.dataframe(df_tag_db)
+
+### Search results
+df_search_result = gt.extract_records_from_db_with_word(word)
+#st.dataframe(df_search_result)
+
+### Count search records
+unique_image_num = df_search_result[gt.tag_db_columns[0]].nunique()
+
+### Define how many images are shown
+continuation_num = min(5, unique_image_num, search_result_N)
+
+if len(df_search_result) <= 0:
+    st.error('No image found')
+else:
+    image_name = gt.tag_db_columns[0]
+    images = list(df_search_result[image_name])
+    col1, col2= st.columns(2)
+    ## Present headers for images & tags
+    with col1:
+        st.write('images')
+    with col2:
+        st.write('tags')
+    ## Present analyzed results
+    for i in range(continuation_num):
+        col1, col2= st.columns(2)
+        ## Show image in the left
+        with col1:
+            st.image(os.path.join(gt.upload_dir, images[i]))
+        ## Show tags in the right
+        with col2:
+            df_search = gt.load_tag_db()
+            df_tag_confidence = df_search[df_search.image_name == images[i]][gt.tag_db_columns[1:]].sort_values(gt.tag_db_columns[2], ascending=False)
+            df_tag_confidence = df_tag_confidence[:gt.topN]
+            st.dataframe(df_tag_confidence)
